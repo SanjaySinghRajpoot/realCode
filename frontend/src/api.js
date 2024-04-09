5e6ecf36ee95c56f302e08cd3465cc255e230db7
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2024-04-09 16:23:40 +0530

    feat: realCode frontend

---
+++ frontend/src/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { LANGUAGE_VERSIONS } from "./constants";
+
+const API = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+export const executeCode = async (language, sourceCode) => {
+  const response = await API.post("/compile", {
+    language: language,
+    code: sourceCode
+    // version: LANGUAGE_VERSIONS[language],
+    // files: [
+    //   {
+    //     content: sourceCode,
+    //   },
+    // ],
+  });
+
+  console.log(sourceCode)
+
+  console.log(response)
+
+  return response.data.output;
+};

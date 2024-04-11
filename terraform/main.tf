6f09bc545a99f9e48cd39a2c27b3e6f9b6991934
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2024-04-11 23:13:22 +0530

    feat: terrafrom setup

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+  backend "s3" {
+    key = "aws/ec2-deploy/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+resource "aws_instance" "servernode" {
+  ami                    = "ami-052efd3df9dad4825"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.maingroup.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = var.private_key
+    timeout     = "4m"
+  }
+  tags = {
+    "name" = "DeployVM"
+  }
+}

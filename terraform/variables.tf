9447ffa8317a3a7d60b8dd00325148cc85fc4059
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2024-04-13 12:15:08 +0530

    feat: var error fixed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,18 @@
 variable "region" {
     default = "ap-south-1"
 }
+
+variable "key_name" {
+  description = "key_name"
+  type        = string
+}
+
+variable "public_key" {
+  description = "public_key"
+  type        = string
+}
+
+variable "private_key" {
+  description = "private_key"
+  type        = string
+}

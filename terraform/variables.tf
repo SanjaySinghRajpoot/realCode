36d76b92d3bd2a21b202430ec9ada97c5c4b2e40
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2024-04-13 10:57:20 +0530

    fix: vars fized

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,11 +2,11 @@ variable "region" {
     default = "ap-south-1"
 }
 variable "public_key" {
-  
+  default = var.public_key
 }
 variable "private_key" {
-  
+  default = var.private_key
 }
 variable "key_name" {
-  
+  default = var.key_name
 }

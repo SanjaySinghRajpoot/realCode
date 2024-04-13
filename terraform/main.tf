98778df0e84740758ce7a502295dedf2911390fc
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2024-04-13 09:16:57 +0530

    feat: adding github action for tf deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,58 @@ resource "aws_instance" "servernode" {
     "name" = "DeployVM"
   }
 }
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-user"
+  role = "ECR-LOGIN-AUTO"
+}
+resource "aws_security_group" "maingroup" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
+
+
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
+
+output "instance_public_ip" {
+  value     = aws_instance.servernode.public_ip
+  sensitive = true
+}

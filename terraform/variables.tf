d5d952e229c48dbb1febc6243904bd084a902154
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2024-04-13 10:58:12 +0530

    fix: vars removed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,3 @@
 variable "region" {
     default = "ap-south-1"
 }
-variable "public_key" {
-  default = var.public_key
-}
-variable "private_key" {
-  default = var.private_key
-}
-variable "key_name" {
-  default = var.key_name
-}
